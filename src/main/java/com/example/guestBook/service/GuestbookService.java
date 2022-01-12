c12ce4cdc0d5f63e229f6f38d1e4fce04aabdc30
J120_안승재 <49035066+getState@users.noreply.github.com>
2022-01-12 14:25:43 +0900

    :loud_sound: feat: GuestbookService 클래스 구현

---
+++ src/main/java/com/example/guestBook/service/GuestbookService.java
@@ -0,0 +1,18 @@
+package com.example.guestBook.service;
+
+import com.example.guestBook.dto.GuestbookDTO;
+import com.example.guestBook.entity.Guestbook;
+
+public interface GuestbookService {
+    Long register(GuestbookDTO dto);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+}

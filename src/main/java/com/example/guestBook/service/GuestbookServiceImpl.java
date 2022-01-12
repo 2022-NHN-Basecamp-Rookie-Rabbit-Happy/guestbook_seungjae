5cb446d021a8e824d87a847f5d7494bd24ad1156
J120_안승재 <49035066+getState@users.noreply.github.com>
2022-01-12 16:15:07 +0900

    :children_crossing: feat: Guestbook 서비스 Implementation 클래스 구현

--- src/main/java/com/example/guestBook/service/GuestbookServiceImpl.java
+++ src/main/java/com/example/guestBook/service/GuestbookServiceImpl.java
@@ -2,11 +2,18 @@ package com.example.guestBook.service;
 
 
 import com.example.guestBook.dto.GuestbookDTO;
+import com.example.guestBook.dto.PageRequestDTO;
+import com.example.guestBook.dto.PageResultDTO;
 import com.example.guestBook.entity.Guestbook;
 import com.example.guestBook.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -29,5 +36,16 @@ public class GuestbookServiceImpl implements GuestbookService{
         repository.save(entity);
 
         return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO){
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
     }
 }

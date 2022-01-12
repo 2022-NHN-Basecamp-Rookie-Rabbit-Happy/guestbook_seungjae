cbe07a2c9c260c97f1cffad816b5b09e2209fd94
J120_안승재 <49035066+getState@users.noreply.github.com>
2022-01-12 13:25:01 +0900

    :page_facing_up: feat: GusetbookRepository 구현 및 테스트 코드 작성

--- src/main/java/com/example/guestBook/repository/GuestbookRepository.java
+++ src/main/java/com/example/guestBook/repository/GuestbookRepository.java
@@ -2,7 +2,8 @@ package com.example.guestBook.repository;
 
 import com.example.guestBook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
     
 }

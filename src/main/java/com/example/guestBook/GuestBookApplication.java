d6ae991925697cb6509a045a608af0fe1f6196f0
J120_안승재 <49035066+getState@users.noreply.github.com>
2022-01-12 10:36:20 +0900

    :hammer: feat: Guestbook enitity 구현

--- src/main/java/com/example/guestBook/GuestBookApplication.java
+++ src/main/java/com/example/guestBook/GuestBookApplication.java
@@ -1,12 +1,13 @@
 package com.example.guestBook;
 
+import com.example.guestBook.entity.BaseEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class GuestBookApplication {
+public class GuestBookApplication extends BaseEntity {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuestBookApplication.class, args);

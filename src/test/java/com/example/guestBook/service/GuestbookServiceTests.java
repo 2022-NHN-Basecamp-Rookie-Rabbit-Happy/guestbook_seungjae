ae3621f91ae75e97ce9e8e200e88911f13c1ab27
J120_안승재 <49035066+getState@users.noreply.github.com>
2022-01-12 16:15:55 +0900

    :children_crossing: feat: 목록 조회 기능 테스트 구혀ㄴ

--- src/test/java/com/example/guestBook/service/GuestbookServiceTests.java
+++ src/test/java/com/example/guestBook/service/GuestbookServiceTests.java
@@ -1,6 +1,9 @@
 package com.example.guestBook.service;
 
 import com.example.guestBook.dto.GuestbookDTO;
+import com.example.guestBook.dto.PageRequestDTO;
+import com.example.guestBook.dto.PageResultDTO;
+import com.example.guestBook.entity.Guestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +12,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class GuestbookServiceTests {
     @Autowired
     private GuestbookService service;
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+        System.out.println("--------------------------");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+        System.out.println("-----------------------------------");
+    }
+
 
     @Test
     public void testRegister(){
